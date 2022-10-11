c7db7b5c367ca98e70740daadb2e2ffff6a98cd8
xmy <947656035@qq.com>
2022-10-11 20:19:30 +0800

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import styles from './index.less';
+import Game from './js/game.jsx';
 
 export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <Game />
     </div>
   );
 }
